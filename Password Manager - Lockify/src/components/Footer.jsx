1a4c7b6fd913b6b02a120bf5514161557733ce63
Ayesha Afzal <154336255+AyeshaAfzal-01@users.noreply.github.com>
2025-02-08 12:35:45 +0500

    Update Footer.jsx

--- Password Manager - Lockify/src/components/Footer.jsx
+++ Password Manager - Lockify/src/components/Footer.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const Footer = () => {
   return (
-    <div className='bg-black w-full flex flex-col gap-2 py-2 absolute bottom-0 justify-center items-center text-white'>
+    <div className='bg-black w-full flex flex-col gap-2 py-2 fixed bottom-0 justify-center items-center text-white'>
       <div className="logo flex justify-center items-center text-2xl gap-2">
         <span className='text-orange-600 font-bold'>&lt;</span>
         <span className='flex justify-center items-center font-bold'><img  width={25} src="key.png" alt="" /> Lockify </span>
